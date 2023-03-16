4bc92af301c9b52b3e5753d3b9afdda6ec342843
Gimillo <jaimegimillo@gmail.com>
2023-03-16 11:37:08 +0100

    Some style errors fixed

--- src/main/java/stringcal/StringCalculator.java
+++ src/main/java/stringcal/StringCalculator.java
@@ -1,34 +1,36 @@
 package stringcal;
 
-
 public class StringCalculator {
-    public int calculator(String arg) {
-        int res = 0;
-        String regex = ",\n";
+  /**
+   * Main calculator.
+   * 
+   * @param arg operation to execute
+   * @return result of operation
+   */
+  public int calculator(String arg) {
+    int res = 0;
+    String regex = ",\n";
 
-        if (arg == null) {
-            return res;
-        }
-
-        if (arg.length() == 1) {
-            return Integer.parseInt(arg);
-        }
-
-        if (arg.length() > 2 && arg.substring(0,2).equals("//")) {
-            regex += arg.charAt(2);
-            arg = arg.substring(3);
-        }
-
-        String[] operands = arg.split("["+regex+"]");
-
-        for (String operand : operands) {
-            int number = Integer.parseInt(operand);
-
-            if (number <= 1000) {
-                res += number;
-            }
-        }
-
-        return res;
+    if (arg == null) {
+      return res;
     }
+
+    if (arg.length() == 1) {
+      return Integer.parseInt(arg);
+    }
+
+    if (arg.length() > 2 && arg.substring(0,2).equals("//")) {
+      regex += arg.charAt(2);
+      arg = arg.substring(3);
+    }
+
+    String[] operands = arg.split("[" + regex + "]");
+    for (String operand : operands) {
+      int number = Integer.parseInt(operand);
+      if (number <= 1000) {
+        res += number;
+      }
+    }
+    return res;
+  }
 }
