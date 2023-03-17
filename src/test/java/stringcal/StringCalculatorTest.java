88bc64ef811276bc5fa652a0a89544c6eee0e2c9
Gimillo <jaimegimillo@gmail.com>
2023-03-17 11:47:46 +0100

    .

--- src/test/java/stringcal/StringCalculatorTest.java
+++ src/test/java/stringcal/StringCalculatorTest.java
@@ -12,12 +12,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StringCalculatorTest {
     private StringCalculator sc = new StringCalculator();
 
-    final int EXPECTED_2 = 2;
-    final int EXPECTED_3 = 3;
-    final int EXPECTED_4 = 4;
-    final int EXPECTED_7 = 7;
-    final int EXPECTED_10 = 10;
-    final int EXPECTED_30 = 30;
+    private final int expected2 = 2;
+    private final int expected3 = 3;
+    private final int expected4 = 4;
+    private final int expected7 = 7;
+    private final int expected10 = 10;
+    private final int expected30 = 30;
 
     @Test
     public void nullTest() {
@@ -31,22 +31,22 @@ public class StringCalculatorTest {
 
     @Test
     public void numCommaNumSumTest() {
-        assertEquals(sc.calculator("2,5"), EXPECTED_7);
-        assertEquals(sc.calculator("9,1"), EXPECTED_10);
-        assertEquals(sc.calculator("0,3"), EXPECTED_3);
+        assertEquals(sc.calculator("2,5"), expected7);
+        assertEquals(sc.calculator("9,1"), expected10);
+        assertEquals(sc.calculator("0,3"), expected3);
     }
 
     @Test
     public void numNewLineNumSumTest() {
-        assertEquals(sc.calculator("4\n3"), EXPECTED_7);
-        assertEquals(sc.calculator("2\n2"), EXPECTED_4);
-        assertEquals(sc.calculator("4\n0"), EXPECTED_4);
+        assertEquals(sc.calculator("4\n3"), expected7);
+        assertEquals(sc.calculator("2\n2"), expected4);
+        assertEquals(sc.calculator("4\n0"), expected4);
     }
 
     @Test
     public void sumThreeNumbersTest() {
-        assertEquals(sc.calculator("1\n0,1"), EXPECTED_2);
-        assertEquals(sc.calculator("9,10\n11"), EXPECTED_30);
+        assertEquals(sc.calculator("1\n0,1"), expected2);
+        assertEquals(sc.calculator("9,10\n11"), expected30);
     }
 
     @Test
