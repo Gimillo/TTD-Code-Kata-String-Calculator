cd634c69603d6b5cf63cb688bf9ebca8a7bf7fc8
Gimillo <jaimegimillo@gmail.com>
2023-03-17 11:58:21 +0100

    .

--- src/main/java/stringcal/StringCalculator.java
+++ src/main/java/stringcal/StringCalculator.java
@@ -13,33 +13,33 @@ public class StringCalculator {
     public int calculator(String arg) {
         int res = 0;
         String regex = ",\n";
-		String str = arg;
-		final int maxlegth = 1000;
-		final int extrapos = 2;
-
+        String str = arg;
+        final int maxlegth = 1000;
+        final int extrapos = 2;
+        
         if (str == null) {
             return res;
         }
-
+        
         if (str.length() == 1) {
             return Integer.parseInt(str);
         }
-
+        
         if (str.length() > 2 && str.substring(0, extrapos).equals("//")) {
             regex += str.charAt(extrapos);
             str = str.substring(extrapos + 1);
         }
-
+        
         String[] operands = str.split("[" + regex + "]");
-
+        
         for (String operand : operands) {
             int number = Integer.parseInt(operand);
-
+            
             if (number <= maxlegth) {
                 res += number;
             }
         }
-
+        
         return res;
     }
 }
